2a6e7c394912dd2ecb11c5c3862a233e6e85f52b
Patrick Wiliams <patrickwilliams520@gmail.com>
2019-04-18 16:25:02 -0400

    Add conversion to MNIST code and progress on extraction code

--- extract_face_data.py
+++ extract_face_data.py
@@ -1,18 +1,19 @@
 import os
 import tensorflow as tf
 
-def list_files(dir):
-    r = []
-    for root, dirs, files in os.walk(dir):
-        for name in files:
-            if name.endswith("pgm"):
-                r.append(os.path.join(root, name))
-    return r
+# def list_files(dir):
+#     r = []
+#     for root, dirs, files in os.walk(dir):
+#         for name in files:
+#             if name.endswith("pgm"):
+#                 r.append(os.path.join(root, name))
+#     return r
 
 def parse(filename, label):
   image_string = tf.read_file(filename)
   image_decoded = tf.image.decode_jpeg(image_string)
   return image_decoded, label
+
 
 path = "CroppedYale/"
 
@@ -23,10 +24,11 @@ for item in (os.walk(path)):
     s = []
     if len(item[2]) > 1:
         for file in item[2]:
-            if file.endswith("pgm"):
-                s.append(file)
+            if file.endswith("jpeg"):
+                s.append(os.path.join(item[0], file))
     if len(s) > 1:
         r.append(s)
+
 
 # print([x for x in r])
 
@@ -41,5 +43,17 @@ for i in range(len(r)):
         filenames.append(file)
         labels3.append(labels2[i])
 
-print(filenames)
-print(labels3)
+filenames = tf.constant(filenames)
+labels = tf.constant(labels3)
+
+dataset = tf.data.Dataset.from_tensor_slices((filenames, labels))
+dataset = dataset.map(parse)
+
+iterator = dataset.make_one_shot_iterator()
+next_element = iterator.get_next()
+
+with tf.Session() as sess:
+    for i in range(100):
+      value = sess.run(next_element)
+      print(value)
+# (X_train, y_train), (X_test, y_test) = dataset
